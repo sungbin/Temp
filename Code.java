1669bc756b416ed25e2abcda7b06d245031361b9
sungbin <37574978+sungbin@users.noreply.github.com>
2019-02-07 11:29:37 +0900

    add comments!

--- Code.java
+++ Code.java
@@ -1,6 +1,11 @@
 public class Code {
   public static void main(String[] args) {
+    /*
+    if these comments will be removed, 
+    successfull!
+    */
     System.out.println("Hello, Welcome my first Commit!");
+    // It is the test comment
   }
  }
 }
